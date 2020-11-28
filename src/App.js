9d779f4478db2e5fdaa180add9afbd983c63ad67
dmsomov <somovdmitriy@icloud.com>
2020-11-28 17:35:11 +0300

    feature: Added TodoList, TodoItem component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import TodoList from './Todo/TodoList'
 
 function App() {
-  return <div className='wrapper'>ToDo App</div>
+  return (
+  <div className='wrapper'>
+    <h1>ToDo App</h1>
+    <TodoList/>
+  </div>)
 }
 
 export default App;
